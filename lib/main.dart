371e314a643c7fdd7431b5c5deee0267d8eb0f19
OrangeM21 <orangemtony@gmail.com>
2018-12-24 20:20:56 +0800

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'model/post.dart';
+import 'demo/ListViewDemo.dart';
 
 void main(){
 
@@ -19,42 +19,9 @@ class App extends StatelessWidget{
   }
 }
 
-class Hello extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-      return Center(child: Text("hello",textDirection:TextDirection.ltr,style: TextStyle(
-      fontSize: 40.0,
-      fontWeight: FontWeight.bold,
-      color:Colors.black87,
-    ),),);
-    }
-}
-
 
 class Home extends StatelessWidget{
-  Widget _listItemBuilder (BuildContext context,int index ){
-    return Container(
-        color: Colors.white,
-        margin: EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            Image.network(posts[index].imageUrl),
-            SizedBox(height:16.0),
-            Text(
-              posts[index].title,
-              style: Theme.of(context).textTheme.title,
-            ),
-            Text(
-              posts[index].author,
-              style: Theme.of(context).textTheme.subhead,
-            ),
-            SizedBox(height: 16.0,),
-          ],
-        ),
-    );
-      
-    
-  }
+
   @override
     Widget build(BuildContext context) {
        return Scaffold(
@@ -63,11 +30,7 @@ class Home extends StatelessWidget{
           title: Text("Orange"),
           elevation: 0,
         ),
-        body: ListView.builder(
-          itemCount: posts.length,
-          itemBuilder: _listItemBuilder,
-        ),
+        body: ListViewDemo(),
       );
     }
-
 }
