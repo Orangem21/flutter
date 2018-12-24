9eb85561ccd439bfee71f8b34b50dac0b490c084
OrangeM21 <orangemtony@gmail.com>
2018-12-24 19:16:36 +0800

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,35 @@ import 'package:flutter/material.dart';
 
 void main(){
 
-  runApp(
-      Center(
-        child:Text(
-          'hello',
-          textDirection:TextDirection.ltr,
+  runApp(App());
+}
+
+
+class App extends StatelessWidget{
+  @override //覆盖父类build方法
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Orange"),
+          elevation: 0,
         ),
-      )
-  );
+        body: Hello(),
+      ),
+      theme: ThemeData(
+        primarySwatch:Colors.yellow, 
+      ),
+    );
+  }
+}
+
+class Hello extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+      return Center(child: Text("hello",textDirection:TextDirection.ltr,style: TextStyle(
+      fontSize: 40.0,
+      fontWeight: FontWeight.bold,
+      color:Colors.black87,
+    ),),);
+    }
 }
