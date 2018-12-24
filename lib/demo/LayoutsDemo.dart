c426e7105de50b0f80889ffe2bf500fedbd90ad5
OrangeM21 <orangemtony@gmail.com>
2018-12-25 04:33:19 +0800

    利用aspectRatio设置宽高的比例

--- lib/demo/LayoutsDemo.dart
+++ lib/demo/LayoutsDemo.dart
@@ -6,53 +6,26 @@ class LayoutsDemo extends StatelessWidget {
 
       return Container(
         child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           // crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             // IconBadge(Icons.pool,size:128.0),
             // IconBadge(Icons.pool,size:128.0),
-            Stack(
-              children: <Widget>[
-              SizedBox(
-              width: 200.0,
-              height: 300.0,
-              child:Container(
-                alignment: Alignment(1.0, 0.0),
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(3, 54, 255, 1.0),
-                  borderRadius: BorderRadius.circular(8.0),
+              AspectRatio(
+                aspectRatio: 3.0/2.0,
+                child: Container(
+                  
+                  color:Color.fromRGBO(3, 54, 255, 1.0)
                 ),
-                child:Icon(Icons.history,color:Colors.white,size:32.0),
               )
-            ),
-            SizedBox(
-              height: 32.0,
-            ),
-           SizedBox(
-              width: 100.0,
-              height: 100.0,
-              child:Container(
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(3, 54, 255, 1.0),
-                  // borderRadius: BorderRadius.circular(8.0),
-                  shape:  BoxShape.circle,
-                  gradient: RadialGradient(colors:[
-                    Color.fromRGBO(7, 102, 255, 1.0),
-                    Color.fromRGBO(3, 54, 255, 1.0),
-                  ] ),
-                ),
-                child:Icon(Icons.person,color:Colors.white,size:32.0),
-              )
-            )
               ],
             ),
-          ],
-        ),
       );
     }
 }
 
 class IconBadge extends StatelessWidget {
+
   final IconData icon;
   final double size;
 
@@ -68,3 +41,5 @@ class IconBadge extends StatelessWidget {
       );
     }
 }
+
+
