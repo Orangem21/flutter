f792fa940766c233bdfedf85285f0c85bd844331
OrangeM21 <orangemtony@gmail.com>
2018-12-25 16:54:46 +0800

    SliverList添加盒子

--- lib/demo/sliverDemo.dart
+++ lib/demo/sliverDemo.dart
@@ -11,7 +11,7 @@ class sliverDemo extends StatelessWidget{
              SliverSafeArea(
                sliver: SliverPadding(
                padding: EdgeInsets.all(8.0),
-               sliver: SliverGridDemo(),
+               sliver: SliverListDemo(),
              ),
              ),
 
@@ -40,6 +40,63 @@ class SliverGridDemo extends StatelessWidget{
                        posts[index].imageUrl,
                        fit:BoxFit.cover,
                      ),
+                   );
+                 },
+                 childCount: posts.length,
+                 ),
+             );
+      }
+}
+class SliverListDemo extends StatelessWidget{
+    @override
+      Widget build(BuildContext context) {
+        // TODO: implement build
+        return SliverList(
+              //  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              //    crossAxisCount: 2,
+              //    crossAxisSpacing: 8.0,
+              //    mainAxisSpacing: 8.0,
+              //    childAspectRatio: 1,
+              //  ),
+               delegate: SliverChildBuilderDelegate(
+                 (BuildContext context,int index){
+                   return Padding(
+                     padding: EdgeInsets.only(bottom: 32.0),
+                     child: Material(
+                       borderRadius: BorderRadius.circular(12.0),
+                       elevation: 14.0,
+                       shadowColor: Colors.grey.withOpacity(0.5),
+
+                       child: Stack(
+                         children: <Widget>[
+                           Positioned(
+                             top: 32.0,
+                             left: 32.0,
+                             child: Column(
+                               crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(posts[index].title,style:TextStyle(
+                                  fontSize: 20.0,
+                                  color: Colors.black,
+                                )),
+                                Text(posts[index].author,style:TextStyle(
+                                  fontSize: 13.0,
+                                  color: Colors.black,
+                                ))
+                              ],
+                             ),
+                           ),
+                           AspectRatio(
+                         aspectRatio: 16/9,
+                          child: Image.network(
+                          posts[index].imageUrl,
+                          fit:BoxFit.cover,
+                            ),
+                          )
+                         ],
+                       ),
+                     ),
+                     
                    );
                  },
                  childCount: posts.length,
