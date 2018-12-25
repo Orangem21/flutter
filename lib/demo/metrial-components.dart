77146ebea8973928cc3219022c31890cc1111fb2
OrangeM21 <orangemtony@gmail.com>
2018-12-26 01:49:48 +0800

    浮动按钮和浮动按钮+文字

--- lib/demo/metrial-components.dart
+++ lib/demo/metrial-components.dart
@@ -19,12 +19,29 @@ class MaterialComponents extends StatelessWidget {
   }
 }
 class FloatingActionButtonDemo extends StatelessWidget {
+  final Widget _floatingActionButton = FloatingActionButton(
+    onPressed: (){},
+    child: Icon(Icons.add),
+    elevation: 0.0,
+    backgroundColor: Colors.black87,
+    shape: BeveledRectangleBorder(
+      borderRadius: BorderRadius.circular(10.0),
+    ),
+  );
+
+  final Widget _floatingActionButtonExtended = FloatingActionButton.extended(
+      onPressed: (){},
+      icon: Icon(Icons.add),
+      label: Text('add'),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('FloatingActionButtonDemo'),
       ),
+      floatingActionButton: _floatingActionButtonExtended,
     );
   }
 }
