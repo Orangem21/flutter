8decf8a5c18ae5c3a2b57d9c2c8af02743dc2af7
OrangeM21 <orangemtony@gmail.com>
2018-12-25 17:02:58 +0800

    fix

--- lib/demo/sliverDemo.dart
+++ lib/demo/sliverDemo.dart
@@ -8,6 +8,9 @@ class sliverDemo extends StatelessWidget{
        return Scaffold(
          body: CustomScrollView(
            slivers: <Widget>[
+             SliverAppBar(
+               title: Text("Orange"),
+             ),
              SliverSafeArea(
                sliver: SliverPadding(
                padding: EdgeInsets.all(8.0),
@@ -47,60 +50,57 @@ class SliverGridDemo extends StatelessWidget{
              );
       }
 }
-class SliverListDemo extends StatelessWidget{
-    @override
-      Widget build(BuildContext context) {
-        // TODO: implement build
-        return SliverList(
-              //  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              //    crossAxisCount: 2,
-              //    crossAxisSpacing: 8.0,
-              //    mainAxisSpacing: 8.0,
-              //    childAspectRatio: 1,
-              //  ),
-               delegate: SliverChildBuilderDelegate(
-                 (BuildContext context,int index){
-                   return Padding(
-                     padding: EdgeInsets.only(bottom: 32.0),
-                     child: Material(
-                       borderRadius: BorderRadius.circular(12.0),
-                       elevation: 14.0,
-                       shadowColor: Colors.grey.withOpacity(0.5),
-
-                       child: Stack(
-                         children: <Widget>[
-                           Positioned(
-                             top: 32.0,
-                             left: 32.0,
-                             child: Column(
-                               crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(posts[index].title,style:TextStyle(
-                                  fontSize: 20.0,
-                                  color: Colors.black,
-                                )),
-                                Text(posts[index].author,style:TextStyle(
-                                  fontSize: 13.0,
-                                  color: Colors.black,
-                                ))
-                              ],
-                             ),
-                           ),
-                           AspectRatio(
-                         aspectRatio: 16/9,
-                          child: Image.network(
-                          posts[index].imageUrl,
-                          fit:BoxFit.cover,
-                            ),
-                          )
-                         ],
-                       ),
+class SliverListDemo extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return SliverList(
+       delegate: SliverChildBuilderDelegate(
+         (BuildContext context, int index) {
+           return Padding(
+             padding: EdgeInsets.only(bottom: 32.0),
+             child: Material(
+               borderRadius: BorderRadius.circular(12.0),
+               elevation: 14.0,
+               shadowColor: Colors.grey.withOpacity(0.5),
+               child: Stack(
+                 children: <Widget>[
+                   AspectRatio(
+                     aspectRatio: 16/9,
+                     child: Image.network(
+                       posts[index].imageUrl,
+                       fit: BoxFit.cover,
                      ),
-                     
-                   );
-                 },
-                 childCount: posts.length,
-                 ),
-             );
-      }
-}
+                   ),
+                   Positioned(
+                     top: 32.0,
+                     left: 32.0,
+                     child: Column(
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       children: <Widget>[
+                         Text(
+                           posts[index].title,
+                           style: TextStyle(
+                             fontSize: 20.0,
+                             color: Colors.white
+                           ),
+                         ),
+                         Text(
+                           posts[index].author,
+                           style: TextStyle(
+                             fontSize: 13.0,
+                             color: Colors.white
+                           ),
+                         ),
+                       ],
+                     ),
+                   ),
+                 ],
+               ),
+             ),
+           );
+         },
+         childCount: posts.length,
+       ),
+     );
+   }
+ }
