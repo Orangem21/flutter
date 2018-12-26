5918106e2f851af18aaaa93481a7cf7b2a472863
OrangeM21 <orangemtony@gmail.com>
2018-12-26 15:00:40 +0800

    简单开关

--- lib/demo/checkbox_demo.dart
+++ lib/demo/checkbox_demo.dart
@@ -12,6 +12,8 @@ String _currentMenuItem  = 'Home';
 
   bool _checkBoxStatusA = true;
 
+  bool _switchItemA = false;
+
   void _handleValueChanged(int value){
     setState(() {
           _radioGroupA = value;
@@ -30,27 +32,27 @@ String _currentMenuItem  = 'Home';
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            RadioListTile(
+            // RadioListTile(
 
-              activeColor: Colors.black,
-              title: Text("Orange"),
-              secondary: Icon(Icons.bookmark),
-              subtitle:Text("Never"),
-              value: 0,
-              groupValue: _radioGroupA,
-              selected: _radioGroupA == 0,
-              onChanged: _handleValueChanged,
-            ),
-            RadioListTile(
-              activeColor: Colors.black,
-              secondary: Icon(Icons.bookmark),
-              title: Text("Orange"),
-              subtitle:Text("Give Up"),
-              value: 1,
-              selected: _radioGroupA == 1,
-              groupValue: _radioGroupA,
-              onChanged: _handleValueChanged,
-            ),
+            //   activeColor: Colors.black,
+            //   title: Text("Orange"),
+            //   secondary: Icon(Icons.bookmark),
+            //   subtitle:Text("Never"),
+            //   value: 0,
+            //   groupValue: _radioGroupA,
+            //   selected: _radioGroupA == 0,
+            //   onChanged: _handleValueChanged,
+            // ),
+            // RadioListTile(
+            //   activeColor: Colors.black,
+            //   secondary: Icon(Icons.bookmark),
+            //   title: Text("Orange"),
+            //   subtitle:Text("Give Up"),
+            //   value: 1,
+            //   selected: _radioGroupA == 1,
+            //   groupValue: _radioGroupA,
+            //   onChanged: _handleValueChanged,
+            // ),
             // CheckboxListTile(
             //   value: _checkBoxStatusA,
             //   onChanged: (value){
@@ -67,6 +69,15 @@ String _currentMenuItem  = 'Home';
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 
+                Text(_switchItemA?':Smile':':Cry',style: TextStyle(color: Colors.black,fontSize: 32.0),),
+                Switch(
+                  value: _switchItemA,
+                  onChanged: (value){
+                    setState(() {
+                      _switchItemA = value;
+                    });
+                  },
+                ),
                 // Radio(
                 //   value: 0,
                 //   groupValue: _radioGroupA,
