591eb1c0f0c519705c7ed7d0afed35f320436ebd
OrangeM21 <orangemtony@gmail.com>
2018-12-26 15:30:14 +0800

    导入intl包

--- lib/demo/checkbox_demo.dart
+++ lib/demo/checkbox_demo.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 
 class checkBoxDemo extends StatefulWidget {
   _checkBoxDemoState createState() => _checkBoxDemoState();
